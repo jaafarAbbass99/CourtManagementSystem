8086d91da894d5a692e7c0941030396ce0e83206
jaafarAbbass99 <jaafarabbass99@gmail.com>
2024-09-18 12:57:47 +0300

    add date to close case

--- app/Models/CaseJudge.php
+++ app/Models/CaseJudge.php
@@ -9,7 +9,7 @@ class CaseJudge extends Model
 {
     use HasFactory;
 
-    protected $fillable = [ 'case_id', 'judge_section_id', 'status', 'base_number', 'full_number', 'is_seen', ];
+    protected $fillable = [ 'case_id', 'judge_section_id', 'status', 'base_number', 'full_number', 'is_seen', 'date_close_case'];
      // العلاقة مع الدعوى 
      public function case() { return $this->belongsTo(Cases::class); } 
      // العلاقة مع القسم القضائي 
